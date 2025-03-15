5dee69eaee8796db3dd0f75aaff5631b00f0cdb1
Awajioniro Emmanuel <eawajioniro@gmail.com>
2025-03-15 11:13:10 +0000

    Switch to PostgreSQL for Railway deployment

--- allroute/settings.py
+++ allroute/settings.py
@@ -107,8 +107,12 @@ WSGI_APPLICATION = "allroute.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR / "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("PGDATABASE", "allroute_db"),
+        "USER": os.environ.get("PGUSER", "allroute_user"),
+        "PASSWORD": os.environ.get("PGPASSWORD", "allroute_pass"),
+        "HOST": os.environ.get("PGHOST", "localhost"),
+        "PORT": os.environ.get("PGPORT", "5432"),
     }
 }
 
